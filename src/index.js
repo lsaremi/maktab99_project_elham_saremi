a792da187021db1be4a2af0a1817b2611587ecef
elham saremi <elhamsaremi88@gmail.com>
2023-12-01 08:27:36 +0330

    feat : add font vazir and config tailwind

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+
+import "./index.css";
+import "tw-elements-react/dist/css/tw-elements-react.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
