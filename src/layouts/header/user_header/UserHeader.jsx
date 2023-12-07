1b93465ef9739a815879c1131f003dde89408264
elham saremi <elhamsaremi88@gmail.com>
2023-12-07 21:01:54 +0330

    feat:add ui site header

--- src/layouts/header/user_header/UserHeader.jsx
+++ src/layouts/header/user_header/UserHeader.jsx
@@ -1,13 +1,22 @@
-import { Outlet } from "react-router-dom";
 import { UserHeaderLink } from "./UserHeaderLink";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/images/logo.png";
+import { HOME_ROUTE } from "../../../config";
 
 export const UserHeader = () => {
   return (
-    <>
-      <header className="flex items-center justify-around">
-        <div>کافی استور</div>
+    <header className="bg-[#4D4D4D]">
+      <div className="container mx-auto w-full flex items-center justify-between">
+        <div className="flex items-center gap-4 relative">
+          <Link to={HOME_ROUTE}>
+            <img width={80} alt="logo" src={logo} className="cursor-pointer" />
+          </Link>
+          <h1 className="text-green-500 font-bold text-xl whitespace-nowrap">
+            کافی استور
+          </h1>
+        </div>
         <UserHeaderLink />
-      </header>
-    </>
+      </div>
+    </header>
   );
 };
--- src/layouts/header/user_header/UserHeaderLink.jsx
+++ src/layouts/header/user_header/UserHeaderLink.jsx
@@ -1,13 +1,37 @@
 import { Link } from "react-router-dom";
+import { BsCart4 } from "react-icons/bs";
+import { OutlineButton } from "../../../components";
+import { BASKET_ROUTE, LOGIN_ROUTE } from "../../../config";
 
 export const UserHeaderLink = () => {
   return (
     <ul className="flex items-center gap-x-2">
       <li>
-        <Link to="login">مدیریت</Link>
+        <Link to={LOGIN_ROUTE}>
+          <OutlineButton
+            bordercolorDark="border-green-700"
+            bordercolorLight="border-green-600"
+            textcolorDark="text-green-200"
+            textcolorLight="text-green-100"
+          >
+            مدیریت
+          </OutlineButton>
+        </Link>
       </li>
       <li>
-        <Link to="basket">سبد خرید</Link>
+        <Link to={BASKET_ROUTE}>
+          <OutlineButton
+            bordercolorDark="border-green-700"
+            bordercolorLight="border-green-600"
+            textcolorDark="text-green-200"
+            textcolorLight="text-green-100"
+          >
+            <div className="flex items-center gap-2">
+              <BsCart4 className="text-base" />
+              سبد خرید
+            </div>
+          </OutlineButton>
+        </Link>
       </li>
     </ul>
   );
