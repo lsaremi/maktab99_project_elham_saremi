26e5238bc5c81724ce04e07bcc5982f5f996ab1e
elham saremi <elhamsaremi88@gmail.com>
2023-12-07 21:22:13 +0330

    feat:add pagination components to handle ui and logic

--- src/components/Base/page_items/PrevPage.jsx
+++ src/components/Base/page_items/PrevPage.jsx
@@ -1,3 +1,17 @@
-export const PrevPage = () => {
-  return <div>PrevPage</div>;
+export const PrevPage = ({ currentPage, onPrevPage }) => {
+  return (
+    <li>
+      <a
+        className={`relative block rounded cursor-pointer ${
+          currentPage === 1
+            ? "text-neutral-400"
+            : "bg-transparent text-neutral-600 hover:bg-neutral-100 dark:hover:bg-neutral-700 dark:text-orange-500 dark:hover:text-orange-500"
+        } px-4 py-1.5 text-lg rounded-full transition-all duration-300`}
+        aria-label="Previous"
+        onClick={onPrevPage}
+      >
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+  );
 };
