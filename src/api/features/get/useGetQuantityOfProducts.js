df374337ce71bdc22528f789fbde4befedcb9da2
elham saremi <elhamsaremi88@gmail.com>
2023-12-07 22:22:12 +0330

    feat:fetch data for stock component and ui

---
+++ src/api/features/get/useGetQuantityOfProducts.js
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { instance } from "../../constants";
+import { PRODUCTS_URL } from "../../../config";
+
+export const useGetQuantityOfProducts = (currentPage = 1, TRowsPerPage = 2) => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["panelQuantityData", currentPage],
+    queryFn: () =>
+      instance
+        .get(`${PRODUCTS_URL}?page=${currentPage}&limit=${TRowsPerPage}`)
+        .then((res) => res.data),
+    keepPreviousData: true,
+    staleTime: 60000,
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  if (data) {
+    const { products } = data.data;
+    const { total, total_pages } = data;
+
+    return [products, total_pages, total];
+  }
+};
