786a7321e65d9266f15a72086237f616a23becf6
elham saremi <elhamsaremi88@gmail.com>
2023-12-29 11:34:41 +0330

    feat:add cartSlice with redux

--- src/features/rootReducer.js
+++ src/features/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
+import cartReducer from "./cart/cartSlice";
+import dateReducer from "./date/dateSlice";
 
 const rootReducer = combineReducers({
+  cart: cartReducer,
   auth: authReducer,
+  date: dateReducer,
 });
 
 export default rootReducer;
