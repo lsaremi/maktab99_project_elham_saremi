7714119d8c48f752504ca82a0b1dd986d013a3a6
elham saremi <elhamsaremi88@gmail.com>
2024-01-05 12:37:01 +0330

    feat:fix pagination for last and first page

--- src/components/Base/page_items/FirstPage.jsx
+++ src/components/Base/page_items/FirstPage.jsx
@@ -1,18 +1,18 @@
-import { GrFormPrevious } from "react-icons/gr";
+import { LuChevronLast } from "react-icons/lu";
 
-export const NextPage = ({ currentPage, totalPages, onNextPage }) => {
+export const FirstPage = ({ onFirstPage, currentPage }) => {
   return (
     <li>
       <a
         className={`relative block rounded cursor-pointer ${
-          currentPage === totalPages
+          currentPage === 1
             ? "text-neutral-400"
             : "bg-transparent text-neutral-600 hover:bg-neutral-100 dark:hover:bg-neutral-700 dark:text-orange-500 dark:hover:text-orange-500"
         } px-4 py-3 text-lg rounded-full transition-all duration-300`}
-        aria-label="Next"
-        onClick={onNextPage}
+        aria-label="Go to First Page"
+        onClick={onFirstPage}
       >
-        <GrFormPrevious />
+        <LuChevronLast />
       </a>
     </li>
   );
