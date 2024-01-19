6332cee25128f797acecdeb5dba585ea711c9d3b
elham saremi <elhamsaremi88@gmail.com>
2024-01-19 10:47:41 +0330

    feat:add and fix privious changes to repo

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import { useState } from "react";
-import payment_gateway from "./assets/images/payment-gateway.jpg";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 function App() {
-  const [paymentResult, setPaymentResult] = useState(null);
-
-  const handlePayment = () => {
-    setPaymentResult("success");
-  };
-
-  const handleCancel = () => {
-    setPaymentResult("fail");
-  };
-
-  const redirectToResultPage = () => {
-    if (paymentResult === "success") {
-      window.location.assign("http://localhost:3000/resultpayment/success");
-    } else if (paymentResult === "fail") {
-      window.location.assign("http://localhost:3000/resultpayment/fail");
-    }
-  };
-
   return (
-    <div className="container">
-      <img
-        src={payment_gateway}
-        alt="payment_gateway"
-        className="payment_gateway"
-      />
-      <div className="btnContainer">
-        <button className="btn fail" onClick={handleCancel}>
-          انصراف
-        </button>
-        <button className="btn success" onClick={handlePayment}>
-          پرداخت
-        </button>
-        {paymentResult && redirectToResultPage()}
-      </div>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import App from "./App";
+
+import "./index.css";
+import "tw-elements-react/dist/css/tw-elements-react.min.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      staleTime: 10000,
+      retry: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
+          <App />
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
